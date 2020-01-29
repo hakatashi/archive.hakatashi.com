99d1d5e435d47149572ed7a34e0fd607155cc5df
Koki Takahashi <hakatasiloving@gmail.com>
2020-01-30 03:16:59 +0900

    Fix production build by setting mode option

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const webpack = require('webpack');
 
 module.exports = {
 	entry: './src/main.js',
+	mode: 'development',
 	output: {
 		path: path.resolve(__dirname, './dist'),
 		publicPath: '/dist/',
@@ -92,20 +93,10 @@ module.exports = {
 };
 
 if (process.env.NODE_ENV === 'production') {
+	module.exports.mode = 'production';
 	module.exports.devtool = '#source-map';
 	// http://vue-loader.vuejs.org/en/workflow/production.html
 	module.exports.plugins = (module.exports.plugins || []).concat([
-		new webpack.DefinePlugin({
-			'process.env': {
-				NODE_ENV: '"production"',
-			},
-		}),
-		new webpack.optimize.UglifyJsPlugin({
-			sourceMap: true,
-			compress: {
-				warnings: false,
-			},
-		}),
 		new webpack.LoaderOptionsPlugin({
 			minimize: true,
 		}),
