6e2263350f5b629237ea027b85fe6029dd77f0f6
Koki Takahashi <hakatasiloving@gmail.com>
2020-02-03 22:36:20 +0900

    Install bulma

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,65 @@
 <template>
-	<div id="app">
-		<img src="./assets/logo.png">
-		<h1>{{msg}}</h1>
-		<h2>Essential Links</h2>
-		<ul>
-			<li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-			<li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-			<li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-			<li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-		</ul>
-		<h2>Ecosystem</h2>
-		<ul>
-			<li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-			<li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-			<li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-			<li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-		</ul>
-
-		<div
-			ref="pswp"
-			class="pswp"
-			tabindex="-1"
-			role="dialog"
-			aria-hidden="true"
-		>
-			<div class="pswp__bg"/>
-			<div class="pswp__scroll-wrap">
-				<div class="pswp__container">
-					<div class="pswp__item"/>
-					<div class="pswp__item"/>
-					<div class="pswp__item"/>
+	<section class="section">
+		<div class="container">
+			<h1>{{entry.text}}</h1>
+			<div class="columns is-mobile is-multiline" itemscope itemtype="http://schema.org/ImageGallery">
+				<div
+					v-for="medium in media"
+					:key="medium.src"
+					class="column is-half-mobile is-one-quarter-fullhd"
+				>
+					<figure
+						class="image is-3by4"
+						itemprop="associatedMedia"
+						itemscope
+						itemtype="http://schema.org/ImageObject"
+					>
+						<a :href="medium.src" itemprop="contentUrl" data-size="600x400">
+							<img :src="medium.src" itemprop="thumbnail">
+						</a>
+					</figure>
 				</div>
+			</div>
 
-				<div class="pswp__ui pswp__ui--hidden">
-					<div class="pswp__top-bar">
-						<div class="pswp__counter"/>
-						<button class="pswp__button pswp__button--close" title="Close (Esc)"/>
-						<button class="pswp__button pswp__button--fs" title="Toggle fullscreen"/>
-						<button class="pswp__button pswp__button--zoom" title="Zoom in/out"/>
-						<div class="pswp__preloader">
-							<div class="pswp__preloader__icn">
-								<div class="pswp__preloader__cut">
-									<div class="pswp__preloader__donut"/>
+			<div
+				ref="pswp"
+				class="pswp"
+				tabindex="-1"
+				role="dialog"
+				aria-hidden="true"
+			>
+				<div class="pswp__bg"/>
+				<div class="pswp__scroll-wrap">
+					<div class="pswp__container">
+						<div class="pswp__item"/>
+						<div class="pswp__item"/>
+						<div class="pswp__item"/>
+					</div>
+
+					<div class="pswp__ui pswp__ui--hidden">
+						<div class="pswp__top-bar">
+							<div class="pswp__counter"/>
+							<button class="pswp__button pswp__button--close" title="Close (Esc)"/>
+							<button class="pswp__button pswp__button--fs" title="Toggle fullscreen"/>
+							<button class="pswp__button pswp__button--zoom" title="Zoom in/out"/>
+							<div class="pswp__preloader">
+								<div class="pswp__preloader__icn">
+									<div class="pswp__preloader__cut">
+										<div class="pswp__preloader__donut"/>
+									</div>
 								</div>
 							</div>
 						</div>
-					</div>
-					<button class="pswp__button pswp__button--arrow--left" title="Previous (arrow left)"/>
-					<button class="pswp__button pswp__button--arrow--right" title="Next (arrow right)"/>
-					<div class="pswp__caption">
-						<div class="pswp__caption__center"/>
+						<button class="pswp__button pswp__button--arrow--left" title="Previous (arrow left)"/>
+						<button class="pswp__button pswp__button--arrow--right" title="Next (arrow right)"/>
+						<div class="pswp__caption">
+							<div class="pswp__caption__center"/>
+						</div>
 					</div>
 				</div>
 			</div>
 		</div>
-	</div>
+	</section>
 </template>
 
 <script>
@@ -67,12 +72,17 @@ export default {
 		return {
 			msg: 'Welcome to Your Vue.js App!!',
 			apikey: localStorage.getItem('HAKATASHI_API_KEY'),
+			media: [],
+			entry: {},
 		};
 	},
 	async mounted() {
 		const pswpElement = this.$refs.pswp;
 		const res = await fetch(`https://co791uc66h.execute-api.ap-northeast-1.amazonaws.com/production/random/twitter?apikey=${this.apikey}`);
 		const data = await res.json();
+
+		this.media = data.media;
+		this.entry = data.entry;
 
 		const options = {
 			index: 0,
@@ -89,30 +99,7 @@ export default {
 @import "../node_modules/photoswipe/dist/photoswipe.css";
 @import "../node_modules/photoswipe/dist/default-skin/default-skin.css";
 
-#app {
-	font-family: 'Avenir', Helvetica, Arial, sans-serif;
-	-webkit-font-smoothing: antialiased;
-	-moz-osx-font-smoothing: grayscale;
-	text-align: center;
-	color: #2c3e50;
-	margin-top: 60px;
-}
-
-h1, h2 {
-	font-weight: normal;
-}
-
-ul {
-	list-style-type: none;
-	padding: 0;
-}
-
-li {
-	display: inline-block;
-	margin: 0 10px;
-}
-
-a {
-	color: #42b983;
+.image img {
+	object-fit: cover;
 }
 </style>
